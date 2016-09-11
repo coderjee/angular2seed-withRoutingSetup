ed139b675c71f17ca96d57888471a7c60309d415
Shubham Verma <developer.shubhamverma@gmail.com>
2016-09-12 01:05:33 +0530

    Routing setup done

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app/app.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
+var core_1 = require('@angular/core');
+var app_module_1 = require('./app.module');
+core_1.enableProdMode();
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
